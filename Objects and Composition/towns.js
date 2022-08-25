4424a675017b2e7267016d0ec35b2d9f568b8fe8
dido1043 <didodido15>
2022-08-25 22:33:59 +0300

    ....

--- Objects and Composition/towns.js
+++ Objects and Composition/towns.js
@@ -14,11 +14,11 @@ function task(arr) {
       .split("|")
       .map((t) => t.trim())
       .filter((x) => x.length != 0);
-      let town = editedArr[0];
-      let latitude = +editedArr[1];
-      let longtitude = +editedArr[2];
-    let towns = new Town(town,latitude.toFixed(2) ,longtitude.toFixed(2) );
-    result.push(towns)
+    let town = editedArr[0];
+    let latitude = +editedArr[1];
+    let longtitude = +editedArr[2];
+    let towns = new Town(town, latitude.toFixed(2), longtitude.toFixed(2));
+    result.push(towns);
   }
   return JSON.stringify(result);
 }
